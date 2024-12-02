9c31d1adbb81fb51b300f410712a2b968338d8ec
thanigaivelan13 <thanigaivelan13@gmail.com>
2024-12-02 17:04:14 +0530

    Fourth Commit

---
+++ src/main/java/assignmentWindows.java
@@ -0,0 +1,40 @@
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class assignmentWindows {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://the-internet.herokuapp.com/");
+		
+		driver.findElement(By.xpath("//a[text()='Multiple Windows']")).click();
+		
+		driver.findElement(By.xpath("//a[text()='Click Here']")).click();
+		
+		Set<String> windows = driver.getWindowHandles();
+		
+		Iterator<String>it = windows.iterator();
+		
+		String parentId = it.next();
+		
+		String childId = it.next();
+		
+		driver.switchTo().window(childId);
+		
+		System.out.println(driver.findElement(By.tagName("h3")).getText());
+		
+		driver.switchTo().window(parentId);
+		
+		System.out.println(driver.findElement(By.tagName("h3")).getText());
+		
+	}
+
+}
