34571c8d2336429c93751bb0d36a56a1e0a10f0e
thanigaivelan13 <thanigaivelan13@gmail.com>
2024-12-02 17:04:32 +0530

    Fourth Commit

--- src/main/java/assignmentWindows.java
+++ src/main/java/assignmentWindows.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class assignmentWindows {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		WebDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
