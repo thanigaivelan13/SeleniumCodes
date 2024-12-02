17beddcd1c61d8d9620c11951486cff8095a6e91
thanigaivelan13 <thanigaivelan13@gmail.com>
2024-12-02 17:14:45 +0530

    Seventh Commit

--- src/main/java/WebdriverScope.java
+++ src/main/java/WebdriverScope.java
@@ -1,4 +1,3 @@
-import java.util.Iterator;
 import java.util.Set;
 
 import org.openqa.selenium.By;
@@ -10,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebdriverScope {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		
 //		System.setProperty("webdriver.chrome.driver", "C:\\Users\\thani\\Downloads\\ChromeDriver\\chromedriver.exe");
 		
