5b15cd27883b8d996271bcac30d72e24e90fff67
thanigaivelan13 <thanigaivelan13@gmail.com>
2024-12-03 21:36:09 +0530

    Ninth Commit

---
+++ src/main/java/ScrollAssignment.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ScrollAssignment {
+    public static void main(String[] args){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        int rowCount = driver.findElements(By.xpath("//table[@name = \"courses\"]/tbody/tr")).size();
+        int columnCount = driver.findElements(By.xpath("//table[@name = \"courses\"]/tbody/tr/th")).size();
+        List<WebElement> rowValues = driver.findElements(By.xpath("//table[@name = \"courses\"]/tbody/tr"));
+        System.out.println("Row Count is: "+rowCount+", Column Count is: "+columnCount);
+        Scanner value = new Scanner(System.in);
+        System.out.println("Enter Row number: ");
+        int rowNumber = Integer.parseInt(value.nextLine());
+        System.out.println("Row Value is:\n" + rowValues.get(rowNumber-1).getText());
+        //System.out.println("Value is: "+rowNumber);
+
+    }
+}
--- src/main/java/Scrolling.java
+++ src/main/java/Scrolling.java
@@ -1,6 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.List;
 
 public class Scrolling {
     public static void main(String[] args){
@@ -11,5 +16,11 @@ public class Scrolling {
         JavascriptExecutor jsExecutor = (JavascriptExecutor)driver;
         jsExecutor.executeScript("window.scrollBy(0, 500)");
         jsExecutor.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+        List<WebElement> values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+        int sum = 0;
+        //System.out.println(Integer.parseInt(value.getText()));
+        for (WebElement value : values) sum = sum + Integer.parseInt(value.getText());
+        int total = Integer.parseInt(driver.findElement(By.cssSelector(".totalAmount")).getText().split(":")[1].trim());
+        Assert.assertEquals(sum, total);
     }
 }
